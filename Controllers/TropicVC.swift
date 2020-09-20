2ec352e8c2a581599f2647b57840e737d36ddf85
visiongirles <baby.do.it@gmail.com>
2020-09-20 22:34:21 +0300

    wip

---
+++ Controllers/TropicVC.swift
@@ -0,0 +1,30 @@
+//
+//  TropicVC.swift
+//  Kate Sept
+//
+//  Created by Max Sychenko on 15.09.2020.
+//  Copyright © 2020 True Devs. All rights reserved.
+//
+
+import UIKit
+
+class TropicVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Controllers/WelcomeVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kate Sept
+//
+//  Created by Max Sychenko on 14.09.2020.
+//  Copyright © 2020 True Devs. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func performSegue (<#parameters#>) -> <#return type#> {
+        <#function body#>
+    }
+
+}
+
